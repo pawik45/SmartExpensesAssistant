2ac9a1241b3583aef9bcbf9365a9bd574d58ec3e
Wasiela <Wasiela@PRYWATNY>
2019-06-01 19:18:52 +0200

    Commit by Sandra Wasiela Category class: gets and addExpense method

--- SEA/src/com/sea/model/Category.java
+++ SEA/src/com/sea/model/Category.java
@@ -1,5 +1,44 @@
 package com.sea.model;
 
-public class Category {
+import java.util.ArrayList;
 
+public class Category {
+	
+	private String name;
+	private ArrayList<Expense> ExpensesList;
+	private double limit;
+	private double balance;
+	
+	public Category(String name,double limit) {
+	
+		this.limit=limit;
+		this.name=name;
+		ExpensesList = new ArrayList<Expense>();
+	}
+	
+	public ArrayList<Expense> getExpensesList() {
+		return this.ExpensesList;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getLimit() {
+		return this.limit;
+	}
+	
+	public double getBalance() {
+		
+		return this.balance;
+	}
+	
+	public void AddExpense(String name,String date, double value) {
+		
+		ExpensesList.add(new Expense(name,date,value));
+
+	}
+	
+
+	
 }
