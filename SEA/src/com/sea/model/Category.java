05c826087c70b60eac31286f33520fe4b45571d8
Paweł <Pawe@DESKTOP-CM95U7O>
2019-06-01 20:37:53 +0200

    reszta Category Category Manager

--- SEA/src/com/sea/model/Category.java
+++ SEA/src/com/sea/model/Category.java
@@ -36,7 +36,24 @@ public class Category {
 	public void AddExpense(String name,String date, double value) {
 		
 		ExpensesList.add(new Expense(name,date,value));
-
+		updateBalance();
+	}
+	
+	public void updateBalance() {
+		
+		double newBalance = 0;
+		
+		for (int i = 0; i < ExpensesList.size(); i++) { 
+			  
+	           	newBalance = newBalance + ExpensesList.get(i).getValue();
+	    } 
+		
+		this.balance=newBalance;
+	}
+	
+	public void changeLimit(double newLimit) {
+		
+		this.limit=newLimit;
 	}
 	
 
--- SEA/src/com/sea/model/CategoryManager.java
+++ SEA/src/com/sea/model/CategoryManager.java
@@ -1,5 +1,43 @@
 package com.sea.model;
 
-public class CategoryManager {
+import java.util.ArrayList;
 
+public class CategoryManager {
+	
+	private double generalBalance;
+	private ArrayList<Category> categoryList;
+	
+	public CategoryManager() {
+		
+		categoryList = new ArrayList<Category>();
+	}
+
+	public double getGeneralBalance() {
+		
+		updateGeneralBalance();
+		return this.generalBalance;
+	}
+	
+	public ArrayList<Category> getCategoryList() {
+		
+		return categoryList;
+	}
+	
+	public void updateGeneralBalance() {
+		
+		double newGeneralBalance = 0;
+		
+		for (int i = 0; i < categoryList.size(); i++) { 
+			  
+	           	newGeneralBalance = newGeneralBalance + categoryList.get(i).getBalance();
+	    } 
+		
+		this.generalBalance = newGeneralBalance;
+	}
+	
+	public void addCategory(String name,double limit) {
+		
+		categoryList.add(new Category(name,limit));
+		updateGeneralBalance();
+	}
 }
