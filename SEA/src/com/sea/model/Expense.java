294f97751395f190e039b20157df35dc30483a6d
Wasiela <Wasiela@PRYWATNY>
2019-06-01 19:06:12 +0200

    Commit by Sandra Wasiela Expense class

--- SEA/src/com/sea/model/Expense.java
+++ SEA/src/com/sea/model/Expense.java
@@ -1,5 +1,43 @@
 package com.sea.model;
 
 public class Expense {
-
+	
+	private String name;
+	private String date;
+	private double value;
+	
+	public Expense(String name,String date,double value) {
+		
+		this.name=name;
+		this.date=date;
+		this.value=value;
+		
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getDate() {
+		return this.date;
+	}
+	
+	public double getValue() {
+		return this.value;
+	}
+	
+	
+	public void changeName(String name) {
+		this.name=name;
+	}
+	
+	public void changeDate(String date) {
+		this.date=date;
+	}
+	
+	public void changeValue(double value) {
+		this.value=value;
+	}
+	
 }
